260b1714debd2c327f821e898b864b140c6b3ebf
CamD67 <7773345+camd67@users.noreply.github.com>
2024-06-01 15:00:28 -0700

    Add player controller comments

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,9 +1,15 @@
 extends CharacterBody2D
 
 
+# Max velocity of the player. Think about this as "top speed".
+# Increase this to increase max speed, decrease to decrease max speed.
 const MAX_VELOCITY = 300.0
-const ACCELERATION = MAX_VELOCITY / 3
-const FRICTION = 35.0
+# Acceleration applied when the player inputs a movement.
+# Decrease the denomenator to "speed up faster", increase the denomenator to "speed up slower"
+const ACCELERATION = MAX_VELOCITY / 3.0
+# Friction applied every frame
+# Decrease the denomenator to slow down faster, increase the denomenator to slow down slower
+const FRICTION = MAX_VELOCITY / 10.0
 
 
 func _physics_process(delta: float) -> void:
