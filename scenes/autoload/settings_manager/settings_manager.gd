6ad5284eda0448906dd7a92464049f65605029c7
CamD67 <7773345+camd67@users.noreply.github.com>
2024-06-09 17:30:31 -0700

    Add JA locale and locale switching

--- scenes/autoload/settings_manager/settings_manager.gd
+++ scenes/autoload/settings_manager/settings_manager.gd
@@ -6,19 +6,42 @@ func _ready() -> void:
 
 ## Load up our player settings
 ## MAKE SURE this funciton loads in the same order as we write
+## Dev note:
+## This should batch up settings into groups of "versions"
+## Each time you add something new, check if it exists first and if not, exit early
 func load_settings_from_disk() -> void:
 	var settings_file := FileAccess.open("user://settings.txt", FileAccess.READ)
 	if settings_file == null:
+		print("Settings file did not exist, loading no settings")
 		return
 
-	settings_file.get_line()
+	var things_loaded: Array[String] = []
+
+	var main_bus_header := settings_file.get_line()
+	if main_bus_header == "":
+		print("Missing initial line of settings file, skipping remainig settings")
+		print("Settings loaded: ", things_loaded)
+		return
+	things_loaded.append(main_bus_header)
 	AudioServer.set_bus_volume_db(AudioConfig.MAIN_BUS_INDEX, linear_to_db(settings_file.get_line().to_float()))
 
-	settings_file.get_line()
+	things_loaded.append(settings_file.get_line())
 	AudioServer.set_bus_volume_db(AudioConfig.SFX_BUS_INDEX, linear_to_db(settings_file.get_line().to_float()))
 
-	settings_file.get_line()
+	things_loaded.append(settings_file.get_line())
 	AudioServer.set_bus_volume_db(AudioConfig.MUSIC_BUS_INDEX, linear_to_db(settings_file.get_line().to_float()))
+
+	var locale_header := settings_file.get_line()
+	if locale_header == "":
+		print("Settings file was missing locale and thus v1, skipping remaining settings")
+		print("Settings loaded: ", things_loaded)
+		return
+	things_loaded.append(locale_header)
+	TranslationServer.set_locale(settings_file.get_line())
+
+
+	# Add more settings loading above this point
+	print("Settings loaded: ", things_loaded)
 
 
 ## Save our player settings
@@ -37,3 +60,6 @@ func flush_settings_to_disk() -> void:
 
 	settings_file.store_line("MusicVolume")
 	settings_file.store_line(str(db_to_linear(AudioServer.get_bus_volume_db(AudioConfig.MUSIC_BUS_INDEX))))
+
+	settings_file.store_line("Locale")
+	settings_file.store_line(TranslationServer.get_locale().split("_")[0])
--- scenes/settings/settings.gd
+++ scenes/settings/settings.gd
@@ -4,12 +4,21 @@ extends Node2D
 @onready var main_volume: OptionsSlider = %MainVolume
 @onready var sfx_volume: OptionsSlider = %SfxVolume
 @onready var music_volume: OptionsSlider = %MusicVolume
+@onready var locale_selector: OptionButton = %LocaleSelector
+
+const locale_by_index := [
+	"en", "ja"
+]
 
 
 func _ready() -> void:
 	main_volume.update_value(db_to_linear(AudioServer.get_bus_volume_db(AudioConfig.MAIN_BUS_INDEX)))
 	sfx_volume.update_value(db_to_linear(AudioServer.get_bus_volume_db(AudioConfig.SFX_BUS_INDEX)))
 	music_volume.update_value(db_to_linear(AudioServer.get_bus_volume_db(AudioConfig.MUSIC_BUS_INDEX)))
+
+	var current_locale_code := TranslationServer.get_locale().split("_")[0]
+	var current_locale_index := locale_by_index.find(current_locale_code)
+	locale_selector.select(current_locale_index)
 
 
 func flush_settings_to_disk() -> void:
@@ -30,3 +39,8 @@ func _on_music_volume_value_change(value: float) -> void:
 
 func _on_back_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu/main_menu.tscn")
+
+
+func _on_locale_selector_item_selected(index: int) -> void:
+	TranslationServer.set_locale(locale_by_index[index])
+	flush_settings_to_disk()
