17f86101e67e3842cb647535cb93c0668149013a
efevans <efevans1@gmail.com>
2024-06-13 17:00:58 -0700

    Add back button to high score menu

--- scenes/high_score/high_score.gd
+++ scenes/high_score/high_score.gd
@@ -1,9 +1,12 @@
 extends Node
 
 @onready var scores_v_box_container: VBoxContainer = %ScoresVBoxContainer
+@onready var button_click: AudioStreamPlayer2D = %ButtonClick
+@onready var back_button: Button = %BackButton
 
 
 func _ready() -> void:
+	back_button.grab_focus.call_deferred()
 	set_scores()
 	
 	
@@ -16,3 +19,9 @@ func set_scores() -> void:
 		var score_label := Label.new()
 		score_label.text = str(score)
 		scores_v_box_container.add_child(score_label)
+
+
+func _on_back_button_pressed() -> void:
+	button_click.play()
+	await button_click.finished
+	get_tree().change_scene_to_file("res://scenes/main_menu/main_menu.tscn")
